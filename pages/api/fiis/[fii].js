160e5bdbc9e037776b3622e6b15899a592baf54f
Lucas Toledo <ltoledo@wkm.com.br>
2021-06-09 17:45:15 -0300

    Screapy Detail

--- pages/api/fiis/[fii].js
+++ pages/api/fiis/[fii].js
@@ -1,12 +1,19 @@
-
-
-
 export default async (req, res) => {
   res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate');
   const { fii } = req.query
   const url = "https://www.fundsexplorer.com.br/funds/"+fii;
   const model = {
-    fii: '#head > div > div > div > div.ticker-wrapper > h1 ($ | trim)'
+    codigo: '#head > div > div > div > div.ticker-wrapper > h1 ($ | trim)',
+    nome: '#head > div > div > div > h3 ($ | trim)',
+    preco: '#stock-price > span.price ($ | trim)',
+    percentagem: '#stock-price > span.percentage ($ | trim)',
+    liquidez_diaria: ' div:nth-child(1) > span.indicator-value ($ | trim)',
+    ultimo_rendimento: ' div:nth-child(2) > span.indicator-value ($ | trim)',
+    dividendo_yield: ' div:nth-child(3) > span.indicator-value ($ | trim)',
+    patrimonio_liquido: ' div:nth-child(4) > span.indicator-value ($ | trim)',
+    valor_patrimonial: ' div:nth-child(5) > span.indicator-value ($ | trim)',
+    rentabilidade_no_mes: ' div:nth-child(6) > span.indicator-value ($ | trim)',
+    p_vp: ' div:nth-child(7) > span.indicator-value ($ | trim)'
   }
 
   const scrapy = require('node-scrapy')
@@ -24,6 +31,14 @@ export default async (req, res) => {
         });
           return;
       }else{
+        result.preco = result.preco.match(/(\d|,)+/g).pop();
+        result.percentagem = result.percentagem.replace("%","")
+        result.ultimo_rendimento = result.ultimo_rendimento.match(/(\d|,)+/g).pop();
+        result.dividendo_yield = result.dividendo_yield.replace("%","")
+        result.patrimonio_liquido = result.patrimonio_liquido.replace("R$ ","")
+        result.valor_patrimonial = result.valor_patrimonial.match(/(\d|,)+/g).pop();
+        result.rentabilidade_no_mes = result.rentabilidade_no_mes.replace("%","")
+
         res.json(result);
       }
   });
